6891e1c2eb61698c3fdce01eaa4715eb8fac7be3
MitsukiNIBAN <psnhewzc@gmail.com>
2018-04-10 20:56:59 +0800

    增加两个界面

---
+++ app/src/main/java/com/satou/wiki/ui/AitemuActivity.java
@@ -0,0 +1,19 @@
+package com.satou.wiki.ui;
+
+import com.satou.wiki.base.BaseActivity;
+
+/**
+ * Created by Mitsuki on 2018/4/10.
+ */
+
+public class AitemuActivity extends BaseActivity{
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected boolean registerEventBus() {
+        return false;
+    }
+}
